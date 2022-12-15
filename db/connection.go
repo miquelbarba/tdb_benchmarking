3b710566b4ce4a6c05a79089630929dbbce10d3a
Miquel Barba <miquel.barba@gmail.com>
2022-12-15 22:21:37 +0100

    Quick and dirty

--- db/connection.go
+++ db/connection.go
@@ -2,21 +2,28 @@ package db
 
 import (
 	"context"
-	"fmt"
-	"os"
 
 	"github.com/jackc/pgx/v5"
 )
 
-func Connection() (*pgx.Conn, context.Context) {
-	ctx := context.Background()
-	connStr := "postgres://postgres:password@192.168.1.36:5432/homework"
+type Connection struct {
+	DB *pgx.Conn
+}
 
+// connStr := "postgres://postgres:password@192.168.1.36:5432/homework"
+func NewConnection(connStr string, ctx context.Context) (Connection, error) {
 	conn, err := pgx.Connect(ctx, connStr)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
-	}
 
-	return conn, ctx
+	return Connection{DB: conn}, err
+}
+
+// SQL query that returns the max cpu usage and min cpu usage of the given hostname
+// for every minute in the time range specified by the start time and end time.
+// SELECT MAX(usage), MIN(usage) FROM cpu_usage WHERE host = $1 AND ts BETWEEN($2, $3)
+func (c *Connection) ExecSelect(ctx context.Context, query string, args ...any) {
+	c.DB.Query(ctx, query, args)
+}
+
+func (c *Connection) Close(ctx context.Context) {
+	c.DB.Close(ctx)
 }
