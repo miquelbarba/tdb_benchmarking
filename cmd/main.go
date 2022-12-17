6d90c92acb84db77133cd8d5652e98bdeb79eccb
Miquel Barba <miquel.barba@gmail.com>
2022-12-17 20:15:12 +0100

    Clean main

--- cmd/main.go
+++ cmd/main.go
@@ -12,21 +12,37 @@ import (
 )
 
 const Query = "SELECT bucket, max, min FROM cpu_usage_summary_minute WHERE host = $1 AND bucket BETWEEN $2 AND $3"
-const connStr = "postgres://postgres:password@192.168.1.36:5432/homework"
+const ConnStr = "postgres://postgres:password@192.168.1.36:5432/homework"
 const NumWorkers = 20
-const File = "data/query_params.csv"
+const FileName = "data/query_params.csv"
 
-func main() {
-	quit := make(chan int)
-	result := make(chan []int64)
+func buildWorkers(numWorkers int) (channels []chan []string, result chan []int64, quit chan int) {
+	quit = make(chan int)
+	result = make(chan []int64)
 
-	var channels [NumWorkers]chan []string
-	for i := 0; i < len(channels); i++ {
-		channels[i] = make(chan []string)
-		go benchmark.Process(connStr, channels[i], result, quit)
+	for i := 0; i < numWorkers; i++ {
+		channels = append(channels, make(chan []string))
+		go benchmark.Process(ConnStr, channels[i], result, quit)
 	}
 
-	f, err := os.Open(File)
+	return channels, result, quit
+}
+
+func quitWorkers(quit chan int, result chan []int64, numWorkers int) []int64 {
+	var totalDuration []int64
+
+	for i := 0; i < numWorkers; i++ {
+		quit <- 0
+
+		durations := <-result
+		totalDuration = append(totalDuration, durations...)
+	}
+
+	return totalDuration
+}
+
+func readCSV(fileName string) [][]string {
+	f, err := os.Open(fileName)
 	if err != nil {
 		log.Panicln(err)
 	}
@@ -40,23 +56,27 @@ func main() {
 		log.Panicln(err)
 	}
 
+	return data
+}
+
+func main() {
+	data := readCSV(FileName)
+	channels, result, quit := buildWorkers(NumWorkers)
+
 	for i := 1; i < len(data); i++ {
 		numChannel, _ := strconv.Atoi(strings.Split(data[i][0], "_")[1])
 		channels[numChannel] <- append([]string{Query}, data[i]...)
 	}
 
-	for i := 0; i < len(channels); i++ {
-		quit <- 0
-	}
+	totalDuration := quitWorkers(quit, result, NumWorkers)
 
-	var totalDuration []int64
-
-	for i := 0; i < len(channels); i++ {
-		durations := <-result
-		totalDuration = append(totalDuration, durations...)
-	}
-
-	fmt.Printf("Number of queries: %d\nAverage: %d\nTotal time: %d\nMin: %d\nMax: %d\nMedian: %d\n",
+	fmt.Printf(
+		"Number of queries: %d\n"+
+			"Average time: %d\n"+
+			"Total time: %d\n"+
+			"Min: %d\n"+
+			"Max: %d\n"+
+			"Median: %d\n",
 		len(totalDuration),
 		benchmark.Average(totalDuration),
 		benchmark.Total(totalDuration),
