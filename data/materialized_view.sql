50329e7adf85b2172323f6b0bae41d0ba6a65767
Miquel Barba <miquel.barba@gmail.com>
2022-12-18 22:21:58 +0100

    Materialized view

---
+++ data/materialized_view.sql
@@ -0,0 +1,9 @@
+\c homework
+CREATE MATERIALIZED VIEW cpu_usage_summary_minute
+WITH (timescaledb.continuous) AS
+SELECT host,
+       time_bucket(INTERVAL '1 minute', ts) AS bucket,
+       MAX(usage),
+       MIN(usage)
+FROM cpu_usage
+GROUP BY host, bucket;
